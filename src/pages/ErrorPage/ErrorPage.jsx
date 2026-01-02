0f675316387238d46bf9b20cc9f85e6abfb5fcb0
KruF <nosol81@gmail.com>
2026-01-02 18:03:53 +0200

    Fixed ErrorPage error message

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -8,7 +8,7 @@ export default function ErrorPage(){
                 Oops! You've encountered an error!
             </h1>
             <p className='errorDescription'>
-                {error.message || error.statusText}
+                {error?.message || error?.statusText || "Something went wrong. Page not found."}
             </p>
             <nav>
                 <NavLink to="/">Return to Home</NavLink>
